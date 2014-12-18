db0573e09d4950942f10f4061b9f2ab79f394565
Dylan Barrell <dylan@barrell.com>
2014-12-18 11:33:45 -0500

    add the check for the module's existence

--- src/nga11yfocus.js
+++ src/nga11yfocus.js
@@ -1,27 +1,34 @@
-angular
-	.module('ngA11y', [])
-	.directive('a11yfocus', function ($timeout) {
-			function hidden(elem) {
-				return !elem.offsetWidth || !elem.offsetHeight
-			}
-			return {
-				restrict: 'A',
-				link: function (scope, element) {
-					$timeout(function () {
-						if (!document.activeElement ||
-							(document.activeElement &&
-								(hidden(document.activeElement) ||
-								document.activeElement.nodeName === 'BODY'))) {
-							// we lost our focus, correct that
-							if (hidden(element[0])) {
-								return;
-							}
-							if (!element[0].hasAttribute('tabindex')) {
-								element[0].setAttribute('tabindex', '0');
-							}
-							element[0].focus();
-						}
-					}, 500);
+(function() {
+	var module;
+	try {
+		module = angular.module('ngA11y');
+	} catch (err) {
+		module = angular.module('ngA11y', []);
+	}
+
+	module.directive('a11yfocus', function ($timeout) {
+				function hidden(elem) {
+					return !elem.offsetWidth || !elem.offsetHeight
 				}
-			};
-		});
+				return {
+					restrict: 'A',
+					link: function (scope, element) {
+						$timeout(function () {
+							if (!document.activeElement ||
+								(document.activeElement &&
+									(hidden(document.activeElement) ||
+									document.activeElement.nodeName === 'BODY'))) {
+								// we lost our focus, correct that
+								if (hidden(element[0])) {
+									return;
+								}
+								if (!element[0].hasAttribute('tabindex')) {
+									element[0].setAttribute('tabindex', '0');
+								}
+								element[0].focus();
+							}
+						}, 500);
+					}
+				};
+			});
+}());
