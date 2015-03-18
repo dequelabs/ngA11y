e7b6036bbbc65e494f851e8c06dafc624ab020e1
Dylan Barrell <dylan@barrell.com>
2015-03-17 21:28:39 -0400

    fix the bower.json file

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,6 +41,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.registerTask('test', [ 'karma:unit' ] );
-    grunt.registerTask('default', 'test');
+    grunt.registerTask('default', ['dist', 'test']);
     grunt.registerTask('dist', 'uglify:dist');
 };
